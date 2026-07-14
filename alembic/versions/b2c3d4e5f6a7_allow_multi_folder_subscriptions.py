148af74bbfbcf20792febcf9ad37b180371dce5e
bilta-keyvalue <bilta@keyvalue.systems>
2026-07-14 14:28:23 +0530

    fix: Sync migration

--- alembic/versions/b2c3d4e5f6a7_allow_multi_folder_subscriptions.py
+++ alembic/versions/b2c3d4e5f6a7_allow_multi_folder_subscriptions.py
@@ -11,7 +11,7 @@ from alembic import op
 
 
 revision: str = "b2c3d4e5f6a7"
-down_revision: Union[str, Sequence[str], None] = "46191d549b50"
+down_revision: Union[str, Sequence[str], None] = "97e6308296b1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
